7c6bb89614de8681ef4354ad4677d33e18df7c15
abhishek-git-9325 <abhishek.mourya@amuratech>
2024-05-23 17:10:43 +0530

    Added frontend URL for cors issue 2

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://ecomstore-frontend.vercel.app/",
+            "https://ecomstore-frontend.vercel.app",
         ],
     })
 );
